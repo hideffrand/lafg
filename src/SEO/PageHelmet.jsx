492f951e4b810e3df6960ad0fc38e45d20469c3a
hideffrand <hideffrand@gmail.com>
2023-10-05 04:26:26 +0700

    test seo

--- src/SEO/PageHelmet.jsx
+++ src/SEO/PageHelmet.jsx
@@ -1,19 +1,26 @@
 import { Helmet } from "react-helmet";
 
-export default function PageHelmet({ title, desc, type, setName }) {
+export default function PageHelmet({ author, title, desc, url }) {
     return (
         <Helmet>
             <title>{title}</title>
+            <base href="https://www.lafg.online" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="author" content={author} />
             <meta name="description" content={desc} />
             
-            <meta property="og:type" content={type} />
+            <meta property="og:url" content={url} />
+            <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={desc} />
+            <meta property="og:image" content="" />
             
-            <meta name="twitter:creator" content={setName} />
-            <meta name="twitter:card" content={type} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content={url} />
+            <meta property="twitter:url" content={url} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={desc} />
+            <meta name="twitter:image" content="" />
         </Helmet>
     )
 }
